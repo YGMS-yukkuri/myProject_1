cbf4c50ab2b970ca2ed0b20f2946405c69c478ee
yukkuri <reityan.sakura0104@gmail.com>
2025-05-14 17:15:21 +0900

    電卓昨日の追加

---
+++ script.js
@@ -0,0 +1,29 @@
+document.addEventListener("keydown",function(e){
+    if(e.key === "Enter"){
+        getAnswer();
+    }
+})
+function getAnswer(){
+    const input1 = Number(document.getElementById("input1").value);
+    const siki = document.getElementById("keisan").value;
+    const input2 = Number(document.getElementById("input2").value);
+    const answer = Keisan(input1,siki,input2);
+    console.log(answer);
+    document.getElementById("AnswerBox").textContent = answer;
+}
+function Keisan(value1,siki,value2){
+    switch(siki){
+        case "pura" :
+            return value1 + value2;
+            break;
+        case "mai" :
+            return value1 - value2;
+            break;
+        case "kake" :
+            return value1 * value2;
+            break;
+        case "wari" :
+            return value1 / value2;
+            break;
+    }
+}
